f083efa9d1c9c17f624087fc27fff0786493ec1d
John Liao <32113215+johnliaogithub@users.noreply.github.com>
2026-02-07 11:12:57 -0500

    added postcss and tailwind files

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '',
+          600: '',
+          700: ''
+        },
+        panel: '',
+        subtle: ''
+      }
+    },
+  },
+  plugins: [],
+}
